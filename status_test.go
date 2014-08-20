a9444013217392b51a0720ec138b82ebb2f79f6f
Todd Bealmear <todd@t0dd.io>
2014-08-19 22:40:17 -0700

    Update format

--- status_test.go
+++ status_test.go
@@ -8,25 +8,25 @@ import (
 )
 
 var _ = Describe("Status", func() {
-  var client Client
-  var status Status
+	var client Client
+	var status Status
 
-  BeforeEach(func() {
-    client, _ = Init(host, port, timeoutms)
-    status, _ = client.GetStatus()
-  })
+	BeforeEach(func() {
+		client, _ = Init(host, port, timeoutms)
+		status, _ = client.GetStatus()
+	})
 
-  AfterEach(func() {
-    status.Free()
-    client.Close()
-  })
+	AfterEach(func() {
+		status.Free()
+		client.Close()
+	})
 
-  Describe("GetSongID", func() {
-    It("Returns the ID of the current song", func() {
-      id, err := status.GetSongID()
+	Describe("GetSongID", func() {
+		It("Returns the ID of the current song", func() {
+			id, err := status.GetSongID()
 
-      Expect(err).ToNot(HaveOccurred())
-      Expect(id).ToNot(Equal(-1)) // MPD returns -1 if no current song
-    })
-  })
+			Expect(err).ToNot(HaveOccurred())
+			Expect(id).ToNot(Equal(-1)) // MPD returns -1 if no current song
+		})
+	})
 })
